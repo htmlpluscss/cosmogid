cf932e2b6b08abd17a3aa4a6a222e582a32a01d9
htmlpluscss <79198889134@ya.ru>
2020-09-13 13:10:51 +0300

    card

---
+++ src/js/buy.js
@@ -0,0 +1,67 @@
+((forms) => {
+
+	if(!forms.length) {
+
+		return;
+
+	}
+
+	Array.from(forms, form => {
+
+		form.addEventListener('submit', event => {
+
+			event.preventDefault();
+
+			const formData = new FormData(form),
+				  xhr = new XMLHttpRequest();
+
+			xhr.open("POST", form.getAttribute('action'));
+
+			xhr.onreadystatechange = () => {
+
+				if (xhr.readyState != 4){
+
+					return;
+
+				}
+
+				if (xhr.status === 200) {
+
+					console.log(xhr.responseText, ' надо возвращать del или add');
+				//	form.elements.mode.value = xhr.responseText;
+
+					form.elements.mode.value = form.elements.mode.value === 'del' ? 'add' : 'del'; // для демо
+
+				}
+
+			}
+
+			xhr.send(formData);
+
+		});
+
+	});
+
+})(document.querySelectorAll('.form-buy'));
+
+// кнопка в один клик
+
+((btns) => {
+
+	if(!btns.length) {
+
+		return;
+
+	}
+
+	Array.from(btns, form => {
+
+		form.addEventListener('click', event => {
+
+			alert('модалка заказа, в нее передаем id');
+
+		});
+
+	});
+
+})(document.querySelectorAll('.buy-one-click'));
