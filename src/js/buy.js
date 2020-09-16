0f79571d7279dc7e1254b49fe866760e0ea9f0fb
htmlpluscss <79198889134@ya.ru>
2020-09-16 11:13:34 +0300

    товар с параметрами

--- src/js/buy.js
+++ src/js/buy.js
@@ -8,6 +8,43 @@
 
 	Array.from(forms, form => {
 
+		const inputID = form.querySelector('[name="id"]'),
+			  inputMode = form.querySelector('[name="mode"]'),
+			  priceText = form.querySelector('.card-product__price-value'),
+			  priceOldText = form.querySelector('.card-product__old-price-value'),
+			  priceOldBox = form.querySelector('.card-product__old-price');
+
+		// кнопки size
+
+		const btnSize = form.querySelectorAll('.form-buy__size-input');
+
+		Array.from(btnSize, input => {
+
+			input.addEventListener('change', () => {
+
+				inputID.value = input.value;
+				priceText.textContent = input.getAttribute('data-price');
+
+				if(input.getAttribute('data-old-price')) {
+
+					priceOldBox.classList.remove('hide');
+					priceOldText.textContent = input.getAttribute('data-old-price');
+
+				}
+				else {
+
+					priceOldBox.classList.add('hide');
+
+				}
+
+				inputMode.value = input.getAttribute('data-in-basket') ? 'del' : 'add';
+
+			});
+
+		});
+
+		// отправка
+
 		form.addEventListener('submit', event => {
 
 			event.preventDefault();
@@ -27,10 +64,33 @@
 
 				if (xhr.status === 200) {
 
-					console.log(xhr.responseText, ' надо возвращать del или add');
+					console.log(xhr.responseText, ' надо возвращать del или add', form.elements.id.value);
 				//	form.elements.mode.value = xhr.responseText;
 
 					form.elements.mode.value = form.elements.mode.value === 'del' ? 'add' : 'del'; // для демо
+
+					if(btnSize.length) {
+
+						Array.from(btnSize, input => {
+
+							if(input.value === form.elements.id.value) {
+
+								if(form.elements.mode.value === 'del') {
+
+									input.setAttribute('data-in-basket','in basket');
+
+								}
+								else {
+
+									input.removeAttribute('data-in-basket');
+
+								}
+
+							}
+
+						});
+
+					}
 
 				}
 
