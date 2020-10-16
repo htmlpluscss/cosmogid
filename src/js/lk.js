0718f57046f8bfbab3bc68c9d9211ce85ff6fc60
htmlpluscss <79198889134@ya.ru>
2020-10-16 09:01:04 +0300

    заказ

---
+++ src/js/lk.js
@@ -0,0 +1,32 @@
+// наведение на строку заказа
+
+((imgs) => {
+
+	"use strict";
+
+	if(!imgs.length) {
+
+		return;
+
+	}
+
+	// первый уровень, второстипенное меню
+	Array.from(imgs, img => {
+
+		img.closest('tr').addEventListener('mousemove', event => {
+
+			img.style.top = event.pageY + 'px';
+			img.style.left = event.pageX + 'px';
+			img.style.opacity = 1;
+
+		});
+
+		img.closest('tr').addEventListener('mouseleave', () => {
+
+			img.style.opacity = 0;
+
+		});
+
+	});
+
+})(document.querySelectorAll('.lk-table-2__img-hover-tr'));
