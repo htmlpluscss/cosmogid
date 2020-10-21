ddb78b64d68384afe3d0995d586438cb80bd8c90
htmlpluscss <79198889134@ya.ru>
2020-10-21 20:19:52 +0300

    Catalog: bar

---
+++ src/js/catalog-bar.js
@@ -0,0 +1,39 @@
+( form => {
+
+	"use strict";
+
+	if(!form) {
+
+		return;
+
+	}
+
+	form.addEventListener('change', () => {
+
+		const formData = new FormData(form),
+			  xhr = new XMLHttpRequest();
+
+		xhr.open("POST", form.getAttribute('action'));
+		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+		xhr.onreadystatechange = () => {
+
+			if (xhr.readyState !== 4){
+
+				return;
+
+			}
+
+			if (xhr.status === 200) {
+
+				console.log(xhr.responseText);
+
+			}
+
+		}
+
+		xhr.send(formData);
+
+	});
+
+})(document.querySelector('.catalog-bar'));
