dfda595d4ebd20452e2f51d09988710c18795de6
htmlpluscss <79198889134@ya.ru>
2022-08-26 15:45:06 +0300

    Найти аналог

--- src/js/events.js
+++ src/js/events.js
@@ -1,13 +1,108 @@
 ( () => {
 
-	document.addEventListener('click', event => {
+	window.addEventListener('click', event => {
 
-/*		const cardVolume = event.target.closest('.preview-card__volume-input');
+// можно унифицировать
 
-		if (cardVolume) {
+		//  Найти аналогичный товар
+
+		const findAnalogue = event.target.closest('.js-find-analogue');
+
+		if (findAnalogue) {
+
+			const template = document.querySelector('#preview-card-row-template').innerHTML;
+
+			document.querySelector('#modal-form-find-analogue-product').innerHTML = Mustache.render(template, {
+
+				id        : findAnalogue.getAttribute('data-id'),
+				articleid : findAnalogue.getAttribute('data-articleid'),
+				link      : findAnalogue.getAttribute('data-link'),
+				price     : findAnalogue.getAttribute('data-price'),
+				priceOld  : findAnalogue.getAttribute('data-price-old'),
+				img       : findAnalogue.getAttribute('data-img'),
+				img2x     : findAnalogue.getAttribute('data-img2x'),
+				name      : findAnalogue.getAttribute('data-name')
+
+			});
 
 		}
-*/
+
+		//  Уведомить  / найти аналог
+
+		const findNotify = event.target.closest('.js-find-notify');
+
+		if (findNotify) {
+
+			const template = document.querySelector('#preview-card-row-template').innerHTML,
+				  product = findNotify.closest('.preview-card'),
+				  productModal = findNotify.closest('#preview-card-color'),
+				  param = {
+					id        : null,
+					articleid : null,
+					link      : null,
+					price     : null,
+					priceOld  : null,
+					img       : null,
+					img2x     : null,
+					name      : null
+				  };
+
+			if ( product ) {
+console.log(product.querySelector('.preview-card__form').elements)
+
+				param.id        = product.querySelector('.preview-card__form').elements.id.value;
+				param.link      = product.querySelector('.preview-card__title').href;
+				param.price     = parseInt(product.querySelector('.preview-card__price-value').textContent);
+				param.img       = product.querySelector('.preview-card__img img').src;
+				param.img2x     = product.querySelector('.preview-card__img img').src;
+				param.name      = product.querySelector('.preview-card__img img').alt;
+
+				if ( product.querySelector('.preview-card__form').elements.articleid ) {
+
+					param.articleid = product.querySelector('.preview-card__form').elements.articleid.value;
+
+				}
+
+				if ( product.querySelector('.preview-card__price-old') ) {
+
+					param.priceOld = parseInt(product.querySelector('.preview-card__price-old').textContent);
+
+				}
+
+			}
+
+			if ( productModal ) {
+console.log(productModal.elements)
+/*<input type="radio" name="articleid" value="1233" class="modal-card-color__item-input visuallyhidden"
+data-src="/img/modal/card-color/468x468.jpg"
+data-srcset="/img/modal/card-color/936x936.jpg 2x"
+data-price="2581 ₽"
+data-buy="disabled"
+data-overlay="not"
+data-overlay-title="Отсутствует до 25.09.2020"
+data-availability="нет в наличии"
+data-availability-color="red">*/
+
+				param.id        = productModal.elements.id.value;
+				param.articleid = productModal.elements.articleid.value;
+				param.link      = productModal.querySelector('.preview-card__title').href;
+				param.price     = parseInt(productModal.elements.articleid.getAttribute('data-price'));
+				param.img       = productModal.elements.articleid.getAttribute('data-src');
+				param.img2x     = productModal.elements.articleid.getAttribute('data-src');
+				param.name      = productModal.querySelector('.modal-card-color__title').textContent;
+
+				if ( productModal.elements.articleid.getAttribute('data-price-old') ) {
+
+					param.priceOld = parseInt(productModal.elements.articleid.getAttribute('data-price-old'));
+
+				}
+
+			}
+
+			document.querySelector('#modal-form-find-notify-product').innerHTML = Mustache.render(template, param);
+
+		}
+
 	});
 
 })();
