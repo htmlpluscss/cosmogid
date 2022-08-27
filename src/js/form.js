c17aa9d6784b5357d4af32aba53fbc842aef452c
htmlpluscss <79198889134@ya.ru>
2022-08-27 22:50:53 +0300

    Акции и скидки

--- src/js/form.js
+++ src/js/form.js
@@ -1,1 +1,47 @@
-autosize(document.querySelectorAll('textarea.autosize'));
+// autosize
+
+	autosize(document.querySelectorAll('textarea.autosize'));
+
+// file
+
+
+( fileupload => {
+
+	if(fileupload.length){
+
+		const template = document.querySelector('#fileupload-template').innerHTML;
+
+		[...fileupload].forEach( file => {
+
+			const input = file.querySelector('[type="file"]'),
+				  btn = file.querySelector('.fileupload__btn');
+
+			input.addEventListener("change", event => {
+
+				btn.classList.add('hide');
+
+				file.insertAdjacentHTML('beforeend', Mustache.render(template, { name : input.value.replace(/.*[\/\\]/, '') }));
+/*
+				console.log('Selected file: ' + input.files.item(0).name);
+				console.log('Selected file: ' + input.files.item(0).size);
+				console.log('Selected file: ' + input.files.item(0).type);
+
+*/			});
+
+			file.addEventListener('click', event => {
+
+				if ( event.target.closest('.fileupload__reset') ) {
+
+					input.value = '';
+					btn.classList.remove('hide');
+					event.target.closest('.fileupload__btn').remove();
+
+				}
+
+			});
+
+		});
+
+	}
+
+})(document.querySelectorAll('.fileupload'));
