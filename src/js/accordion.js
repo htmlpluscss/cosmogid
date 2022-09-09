67aca85f07c8019ff9fe37c97eb35bc0fda6a811
htmlpluscss <79198889134@ya.ru>
2022-09-09 13:30:38 +0300

    Оплата

---
+++ src/js/accordion.js
@@ -0,0 +1,65 @@
+( items => {
+
+	if(!items.length) {
+
+		return;
+
+	}
+
+	[...items].forEach( accordion => {
+
+		let animateOn = false,
+			activeItem = null;
+
+		const items = accordion.querySelectorAll('.accordion__item');
+
+		[...items].forEach( item => {
+
+			const btn = item.querySelector('.accordion__btn'),
+				  head = item.querySelector('.accordion__head'),
+				  body = item.querySelector('.accordion__body'),
+				  arrow = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+			arrow.setAttributeNS(null, "viewBox", "0 0 24 24");
+			arrow.setAttributeNS(null, "width", 24);
+			arrow.setAttributeNS(null, "height", 24);
+			arrow.innerHTML = '<use xlink:href="#svg-arrow-chevron-right"/>';
+
+			head.append(arrow);
+
+			btn.addEventListener('click', () => {
+
+				animateOn = true;
+
+				if( item === activeItem ){
+
+					item.classList.remove('is-open');
+					activeItem = null;
+
+				} else {
+
+					activeItem = item;
+
+					[...items].forEach( el => el.classList.toggle('is-open', el === item));
+
+				}
+
+			});
+
+			body.addEventListener(window.cssAnimation('transition'), () => {
+
+				if(animateOn && activeItem && !window.isInViewport(head)){
+
+					head.scrollIntoView({ behavior: 'smooth' });
+
+				}
+
+				animateOn = false;
+
+			});
+
+		});
+
+	});
+
+})(document.querySelectorAll('.accordion'));
